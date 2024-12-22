47cab5c990c12554941b4620543c09891806b97e
Sanshray06 <sanshraymittu6@gmail.com>
2024-12-22 11:37:26 +0530

    Further process

---
+++ Backend/models/productModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+    name:{type:String , required:true},
+    description:{type:String , required:true},
+    price:{type:Number , required:true},
+    originalPrice:{type:Number, required:true},
+    images:{type:Array, required:true},
+    Mainimage:{type:Array, required:true},
+    category:{type:String , required:true},
+    type:{type:String , required:true},
+    sizes:{type:Array},
+    bestSeller:{type:Boolean},
+    date:{type:Number , required:true},
+})
+
+const productModel = mongoose.models.product || mongoose.model("product",productSchema);
+export default productModel
+
+// id: 1,
+// name: "Classic Necklace Set",
+// price: 1200,
+// originalPrice:2500,
+// currency: '₹',
+// description: "Elegant necklace set with matching earrings.",
+// mainImage: [Product1, mainProduct1_1],
+// images: [
+// mainProduct1_1,
+// mainProduct1_2,
+// mainProduct1_3,
+// mainProduct1_4,
+// ],
+// category: "Women",
+// type: "Necklace",
+// inStock: true,
+// bestseller: true,
